b0caf2b60016b5c36dd6fe7a3dadffa89072d9aa
nparkerAuto <118399799+nparkerAuto@users.noreply.github.com>
2022-12-21 10:14:26 -0500

    Update DataCleanseAutoMain.py
    Added Chemical and Fertilizer Micro cleansing logic.
    Also added a timer which times how long the code runs

--- DataCleanseAutoMain.py
+++ DataCleanseAutoMain.py
@@ -1,44 +1,80 @@
-import os, glob
 import pandas as pd
+import time
 import datetime
-from DataCleanseHelper import prof_char_dict, prof_abbrev_dict, sap_abbrev_dict 
-from DataCleanseHelper import format1_cleanse, format2_cleanse, format3_cleanse
+from AbbrevAndChar import prof_char_dict, prof_abbrev_dict, sap_abbrev_dict,findItem
+from Formats import format1_cleanse, format2_cleanse, format3_cleanse, format_chem, format_Fert_Micro
 
-format1={"203_CORN":"M_SEED_CORN", "203_SOYB":"M_SEED_SOYBEANS", "203_CNLA":"M_SEED_CANOLA","203_SUNF":"M_SEED_SUNFLOWER"}
-format2={"203_FLAX":"M_SEED_FLAX","203_GRSS":"M_SEED_GRASS","203_TRCL":"M_SEED_TRITICALE", "203_RYE":"M_SEED_RYE", "203_OTHR":"M_SEED_OTHER", "203_PEAS":"M_SEED_PEAS", "203_ALFL":"M_SEED_ALFALFA", "203_COTN": "M_SEED_COTTON","203_DURM":"M_SEED_DURUM","203_NVYB":"M_SEED_NAVY_BEANS", "203_SGRB":"M_SEED_SUGAR_BEETS", "203_GRBR":"M_SEED_GRASS","203_GRKB":"M_SEED_GRASS","203_GRPT":"M_SEED_GRASS","203_GRFS":"M_SEED_GRASS","203_GRRY":"M_SEED_GRASS","203_GROR":"M_SEED_GRASS","203_GRWT":"M_SEED_GRASS","203_GRTI":"M_SEED_GRASS","203_GRBG":"M_SEED_GRASS","203_GRCN":"M_SEED_GRASS"}
-format3={"203_BRLY":"M_SEED_BARLEY", "203_WHET":"M_SEED_WHEAT", "203_OATS":"M_SEED_OATS", "203_SORG":"M_SEED_SORGHUM", "203_WHTS":"M_SEED_WHEAT","203_WHTC":"M_SEED_WHEAT","203_WHTW":"M_SEED_WHEAT","203_WHTW":"M_SEED_WHEAT","203_WHSW":"M_SEED_WHEAT",}
-itemID={"203":"SEED"}
+format1=["203_CORN", "203_SOYB", "203_CNLA","203_SUNF","203_SWTC"]
+format2=["203_FLAX","203_GRSS","203_TRCL", "203_RYE", "203_OTHR", "203_PEAS", "203_ALFL", "203_COTN","203_DURM","203_NVYB", "203_SGRB", "203_GRBR","203_GRKB","203_GRPT","203_GRFS","203_GRRY","203_GROR","203_GRWT","203_GRTI","203_GRBG","203_GRCN","203_GRPU"]
+format3=["203_BRLY", "203_WHET", "203_OATS", "203_SORG", "203_WHTS","203_WHTC","203_WHTW","203_WHSW","203_SGSD"]
 
-df_import=pd.read_excel('C:\\Users\\Nora.Parker\\Documents\\SampleFile_4.xlsx')
+itemID={"203":"SEED", "200":"FERT_BULK", "201":"CHEM", "202":"FERT_MICRO", "143":"FEED"}
 
+start_time = time.time()
 
-char_output=[]	
-for index, row in df_import.iterrows():
-	noun = itemID[str(row["ItemIDPart1"])]
-	legacy_num=row["AGRISItemID"]
+df_import=pd.read_excel('C:\\Users\\NParker\\Documents\\SampleFile_5.xlsx')
+
+char_output=[]
+counter=0
+cleansedCounter=0
 	
-	#checking to see if desc follows standards
-	try:
-		if row["Name"][4] == " ":
-			#If standards are followed then unique ID is made to map to SAP characteristics
-			nounID=row["ItemIDPart1"]
-			nounMod=row["Name"][:4]
-			prodID=str(nounID)+"_"+nounMod
-			if prodID in format1:
-				char_output_1=format1_cleanse(row, noun, nounMod, legacy_num, format1[prodID])
-				char_output+=char_output_1
-			elif prodID in format2:
-				char_output_1=format2_cleanse(row, noun, nounMod, legacy_num, format2[prodID])
-				char_output+=char_output_1
-			elif prodID in format3:
-				char_output_1=format3_cleanse(row, noun, nounMod, legacy_num, format3[prodID])
-				char_output+=char_output_1
-		else:
-			pass
-	except IndexError:
-		char_output+= [{"Legacy Number":legacy_num, "Characteristic":row["Name"], "Characteristic Description":"DOES NOT FOLLOW FORMAT: INDEX NAME","Short Description (SAP)": "DOES NOT FOLLOW FORMAT", "Long Description (SAP)":"DOES NOT FOLLOW FORMAT", "PO Description (SAP)":"DOES NOT FOLLOW FORMAT"}]
+for index, row in df_import.iterrows():
+    counter+=1
+    noun = itemID[str(row["ItemIDPart1"])]
+    legacy_num=row["AGRISItemID"]
+	
+    #checking to see if desc follows standards
+    try:
+        if noun=="SEED":
+            if row["Name"][4] == " ":
+                #If standards are followed then unique ID is made to map to SAP characteristics
+                nounID=row["ItemIDPart1"]
+                nounMod=row["Name"][:4]
+                prodID=str(nounID)+"_"+nounMod
+                
+                noun,noun_Mod,sap_class=findItem(nounMod)
+                
+                if prodID in format1:
+                    char_output_1=format1_cleanse(row, noun, noun_Mod, legacy_num, sap_class)
+                    if not char_output_1[0]["Characteristic Description"].startswith("DOES"):
+                        cleansedCounter+=1
+                    char_output+=char_output_1
+                elif prodID in format2:
+                    char_output_1=format2_cleanse(row, noun, noun_Mod, legacy_num, sap_class)
+                    if not char_output_1[0]["Characteristic Description"].startswith("DOES"):
+                        cleansedCounter+=1
+                    char_output+=char_output_1
+                elif prodID in format3:
+                    char_output_1=format3_cleanse(row, noun, noun_Mod, legacy_num, sap_class)
+                    if not char_output_1[0]["Characteristic Description"].startswith("DOES"):
+                        cleansedCounter+=1
+                    char_output+=char_output_1
+            else:
+                char_output+= [{"Legacy Number":legacy_num, "Characteristic":row["Name"], "Characteristic Description":"DOES NOT FOLLOW FORMAT: NOUN NOT FOUND","Short Description (SAP)": "DOES NOT FOLLOW FORMAT", "Long Description (SAP)":"DOES NOT FOLLOW FORMAT", "PO Description (SAP)":"DOES NOT FOLLOW FORMAT"}]
+        if noun=="FERT_MICRO":
+            sap_class="M_FERT_MICRO_DRY"
+            if row["ProductType"]=='052':
+                sap_class='M_FERT_MICRO_LIQ'
+            char_output_1=format_Fert_Micro(row, legacy_num,sap_class)
+            if not char_output_1[0]["Characteristic Description"].startswith("DOES"):
+                cleansedCounter+=1
+            char_output+=char_output_1
+        if noun=="FERT_BULK":
+            pass
+        if noun=="CHEM":
+            char_output_1=format_chem(row, legacy_num)
+            if not char_output_1[0]["Characteristic Description"].startswith("DOES"):
+                cleansedCounter+=1
+            char_output+=char_output_1
+        if noun=="FEED":
+            pass
+    except IndexError:
+        char_output+= [{"Legacy Number":legacy_num, "Characteristic":row["Name"], "Characteristic Description":"DOES NOT FOLLOW FORMAT: INDEX ERROR","Short Description (SAP)": "DOES NOT FOLLOW FORMAT", "Long Description (SAP)":"DOES NOT FOLLOW FORMAT", "PO Description (SAP)":"DOES NOT FOLLOW FORMAT"}]
+       
 
 x = datetime.datetime.now()
-dateString=str(x.year)+str(x.month)+str(x.day)
+dateString=str(x.year)+str(x.month)+str(x.day)+"-"+str(x.second)
 updateFileName="Profisee_Desc_OutPut_"+dateString
-pd.DataFrame.from_dict(char_output).to_csv(f"C:\\Users\\Nora.Parker\\Documents\\{updateFileName}.csv")
+pd.DataFrame.from_dict(char_output).to_excel(f"C:\\Users\\NParker\\Documents\\OutputFiles\\{updateFileName}.xlsx")
+print("My program took", int(time.time() - start_time), "seconds to run")
+print(f"Out of {counter} entries {cleansedCounter} were fully cleansed ({int(((cleansedCounter)/counter)*100)}%)")
